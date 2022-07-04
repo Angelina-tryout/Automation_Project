099399d6893d497c9b95f13decdb3f7f52fb776a
root <root@DESKTOP-P6P1R14.localdomain>
2022-07-05 03:35:45 +0530

    Added automation sh file with Task 3 changes

--- automation.sh
+++ automation.sh
@@ -1,20 +1,20 @@
-
 echo ">>>>apt update:"
 apt update -y
 echo ">>>>> Installing aws cli"
 apt install awscli -y
+apt install cron
 
-#Variables
+
 s3_bucket=upgrad-angelina
 name=angelina
 DateTime=$(date '+%d%m%Y-%H%M%S')
 
-#Apache Check on Installation | Active | Enabled
 echo ">>>>Checking whether apache installed or not"
 dpkg --get-selections | grep apache2 > /dev/null 2>&1
 if [ $? != 0 ]
 then
-		apt install apache2 -y
+
+	apt install apache2 -y
 fi
 
 service apache2 start
@@ -22,18 +22,59 @@ echo ">>>>Checking whether apache2 is running or not"
 service apache2 status | grep "active (running)" > /dev/null 2>&1
 if [ $? != 0 ]
 then
-		service apache2 start > /dev/null
+	service apache2 start > /dev/null
 fi
 
 echo ">>>>>Checking whether apcehe2 is enabled or not"
-systemctl is-active apache2 | grep "enabled" > /dev/null 2>&1
+systemctl is-enabled apache2 | grep "enabled" > /dev/null 2>&1
 if [ $? != 0 ]
 then
-		systemctl enable apache2
-		service apache2 restart > /dev/null
+	systemctl enable apache2
+	service apache2 restart > /dev/null
 fi
+
+
 
 tar -cvf /tmp/${name}-httpd-logs-${DateTime}.tar /var/log/apache2/error.log /var/log/apache2/access.log
 
 aws s3 cp /tmp/${name}-httpd-logs-${DateTime}.tar s3://${s3_bucket}/${name}-httpd-logs-${DateTime}.tar
 
+if [ -f "/var/www/html/inventory.html" ];
+then
+	echo "Inventory exists"
+	size=$(du -s -h /tmp/$name-httpd-logs-$DateTime.tar | awk '{print $1}')
+	echo "<p>httpd-logs&emsp;$DateTime&emsp;&emsp;logs&emsp;$size</p>" >> /var/www/html/inventory.html
+else
+	echo "Inventory does not exists. Creating Inventory..."
+        touch /var/www/html/inventory.html
+	printf "<p>" >> /var/www/html/inventory.html
+B	echo "<p>Log Type&emsp;Date Created&emsp; &emsp;&emsp; Type&emsp;Size</p>" >> /var/www/html/inventory.html
+	size=$(du -s -h /tmp/$name-httpd-logs-$DateTime.tar | awk '{print $1}')
+        echo "<p>httpd-logs&emsp;$DateTime&emsp;&emsp;logs&emsp;$size</p>" >> /var/www/html/inventory.html
+	printf "</p>" >> /var/www/html/inventory.html
+fi
+
+service cron start
+echo ">>>>Checking whether cron is running or not"
+service cron status | grep "active (running)" > /dev/null 2>&1
+if [ $? != 0 ]
+then
+        service cron start > /dev/null
+fi
+
+echo ">>>>>Checking whether cron is enabled or not"
+systemctl is-enabled cron | grep "enabled" > /dev/null 2>&1
+if [ $? != 0 ]
+then
+        systemctl enable cron
+        service cron restart > /dev/null
+fi
+
+if [ -f "/etc/cron.d/automation" ];
+then
+	echo "cron job available"
+else
+	echo "Creating cron job to run every day..."
+	touch /etc/cron.d/automation
+	echo "0 0 * * * root /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+fi
